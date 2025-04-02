09e06193927fba087a71ba3af5ca656f49ff8e94
CoddingNinja <ahmedhaseeb93338@gmail.com>
2025-04-02 21:39:46 +0500

    Click Event  is added

--- AutoCompleteCounties/sript.js
+++ AutoCompleteCounties/sript.js
@@ -2,41 +2,44 @@ document.addEventListener("DOMContentLoaded", function () {
     const Input = document.getElementById("Search-country");
     const Counties = document.getElementById("autocomplete-list");
     const url = "https://usmanlive.com/wp-json/api/countries?q=";
-    const loder=document.getElementById("Loder");
-    let Controller=new AbortController();
+    const loder = document.getElementById("Loder");
+    let Controller = new AbortController();
 
     Input.addEventListener("input", function () {
         let value = Input.value.trim();
 
 
         Controller.abort()
-        Controller=new AbortController();
+        Controller = new AbortController();
 
         if (value === "") {
             Counties.style.display = "none";
-            loder.style.display="none";
+            loder.style.display = "none";
             return;
         }
-        loder.style.display="block";
+        loder.style.display = "block";
 
         fetch(url + value, { signal: Controller.signal })
             .then(res => {
                 return res.json();
             })
             .then(data => {
-                Counties.innerHTML = ""; 
+                Counties.innerHTML = "";
 
                 if (data.length > 0) {
                     data.forEach(element => {
                         let div = document.createElement("div");
                         div.classList.add("Fetch-list-item");
-                        div.textContent = element; 
+                        div.textContent = element;
                         Counties.appendChild(div);
+
+
+
                     });
-                    loder.style.display="none";
+                    loder.style.display = "none";
                     Counties.style.display = "block";
                 } else {
-                    Counties.innerHTML = ""; 
+                    Counties.innerHTML = "";
                     Counties.style.display = "none";
                 }
             })
@@ -44,4 +47,11 @@ document.addEventListener("DOMContentLoaded", function () {
                 console.log("Error fetching data:", error);
             });
     });
+    document.addEventListener("click", function () {
+        Counties.style.display = "none"
+    })
+    Counties.addEventListener("click",function(event){
+        Input.value=event.target.textContent;
+        Counties.style.display="none";
+    })
 });
